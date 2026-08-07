8e3903e17fe97507e6b6011c230e99c71da822e0
monutanti1234 <monutanti1234@gmail.com>
2026-08-07 13:54:39 +0530

    Fix: Use configured API instance for authentication requests

--- devpulse/frontend/src/api/axios.js
+++ devpulse/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Apne backend ka exact port/route check kar lena
+  baseURL:import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api', // Apne backend ka exact port/route check kar lena
 });
 
 // Automatically attaches Authorization header if token exists in localStorage
